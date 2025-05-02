941576a8c4946be170e928facabc882745741553
plthabangr <rakgoropothabang@gmail.com>
2025-05-02 15:43:04 +0200

    Added the create product, delete, get[roduct by id controllers

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -23,3 +23,78 @@ await newProduct.save()
     }
     
 }
+
+
+export const deleteProduct=async(req,res)=>{
+    //Get product using req.body
+    const {id}= req.params
+
+    console.log(id)
+  
+   
+
+    //check the database
+    try{
+        const product=await Product.findById(id)
+
+        if(!product){
+            return res.status(404).json({error:"Product not found"})
+        }
+        //Delete user from database
+ await Product.findByIdAndDelete(id)
+//return results to client if save was a success
+    res.status(200).json({suceess:true,message:"Product deleted"})
+    }catch(error){
+        //show error for debugging purpose
+        console.error("Error in deleting the product:",error.message)
+        //Return error to client
+        res.status(500).json({suceess:false,message:"Server error"})
+    }
+    
+}
+
+
+
+export const getAllProducts=async(req,res)=>{
+    //check the database
+    try{
+        
+        //find all products user from database
+       const  products =await Product.find({})
+//return results to client if save was a success
+    res.status(200).json({suceess:true,data:products})
+    }catch(error){
+        //show error for debugging purpose
+        console.error("Error in finding the products:",error.message)
+        //Return error to client
+        res.status(500).json({suceess:false,message:"Server error"})
+    }
+    
+}
+
+export const getProductByID=async(req,res)=>{
+    //Get product using req.body
+    const {id}= req.params
+
+    console.log(id)
+  
+   
+
+    //check the database
+    try{
+        const product=await Product.findById(id)
+
+        if(!product){
+            return res.status(404).json({error:"Product not found"})
+        }
+ 
+//return results to client if save was a success
+    res.status(200).json({suceess:true,data:product})
+    }catch(error){
+        //show error for debugging purpose
+        console.error("Error in deleting the product:",error.message)
+        //Return error to client
+        res.status(500).json({suceess:false,message:"Server error"})
+    }
+    
+}
--- backend/router/productRouter.js
+++ backend/router/productRouter.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { createProduct } from "../controller/productController.js";
+import { createProduct, deleteProduct,getAllProducts ,getProductByID} from "../controller/productController.js";
 
 
 const router = express.Router()
 
 //create products
 router.post("/add",createProduct)
+router.delete("/:id",deleteProduct)
+router.get("/",getAllProducts)
+router.get("/:id",getProductByID)
 
 export {router as productRouter}
