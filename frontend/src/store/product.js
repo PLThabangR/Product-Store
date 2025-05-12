404f159ce052bf6847508a3c9b54695a158bdb9f
plthabangr <rakgoropothabang@gmail.com>
2025-05-12 14:10:24 +0200

    Creating the Post method using Zustand api to communicate with the backend

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -3,7 +3,21 @@ import {create} from 'zustand'
 
 export const useProductStore=create((set)=>({
     products:[],
-    setProducts:(products)=>set({products})
+    setProducts:(products)=>set({products}),
+    createProducts:async (newProduct)=>{
+        if(!newProduct || !newProduct|| !newProduct){
+            return {success:false,message:"Please fill in all fields. "}
+        }
+        const res = await fetch("/api/products",{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(newProduct)
+        },)
+        //Respond
+        const data = await res.json();
+        //Use Zustand to set state
+        set((state)=>({products:[...state.products,data.product]}))
+    },
 }))
 
 //const [state,setState] = useState([])
