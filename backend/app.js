ba11ee14d359eee1bde8298305eb410e8322d2dd
plthabangr <rakgoropothabang@gmail.com>
2025-05-14 01:12:10 +0200

    Enabling cors in express to allow backend calls

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import { connectDB } from '../backend/config/db.js';
-
+import cors from 'cors'
 import { productRouter } from '../backend/router/productRouter.js';
 //Import path so we worki with files
 import path from "path"
@@ -14,6 +14,7 @@ dotenv.config()
 const app = express()
 //Allow us to work with json data
 app.use(express.json())
+app.use(cors());
 
 app.use("/api/product",productRouter)
 
@@ -31,6 +32,14 @@ const port = process.env.PORT || 3000
 //     res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
 // })
 // }
+
+// //Allow requests
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "https://product-store-1-o1ge.onrender.com"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+
 
 app.listen(port,()=>{
     //Call the connect db function 
